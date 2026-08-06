5123bcf89417ef1ab6dd2cd5cd2e3b0d59dfdc22
dhexny <dhexny18@gmail.com>
2026-08-07 05:21:43 +0800

    fix: remove accidentally committed node_modules and backend files

--- src/routes/auth.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { loginAdmin, registerAdmin } = require("../controller/authController");
-
-router.post("/login", loginAdmin);
-router.post("/register-admin", registerAdmin);
-
-module.exports = router;
